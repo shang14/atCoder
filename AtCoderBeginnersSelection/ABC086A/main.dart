0c82b8ceeb080dd6c60471b89d3ca399849cc321
shang14 <shang14@gmail.com>
2022-11-21 12:32:05 +0800

    modified:   AtCoderBeginnersSelection/ABC086A/main.dart

--- AtCoderBeginnersSelection/ABC086A/main.dart
+++ AtCoderBeginnersSelection/ABC086A/main.dart
@@ -1,1 +1,11 @@
+import 'dart:io';
 
+void main(List<String> args) {
+  final nums = stdin.readLineSync().split(" ").map(int.parse).toList();
+  final product = nums[0] * nums[1];
+  if (product % 2 == 0) {
+    print("Even");
+  } else {
+    print("Odd");
+  }
+}
