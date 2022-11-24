5989e53e50bea991cf3ceca1c953592b54f348ed
shang14 <shang14@gmail.com>
2022-11-24 10:39:59 +0800

    new file:   AtCoderBeginnersSelection/ABC081A/main.cpp

---
+++ AtCoderBeginnersSelection/ABC081A/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num = 0;
+    string masu;
+    cin >> masu;
+    for (int i = 0; i < 3; i++)
+    {
+        if (masu.at(i) == '1')
+        {
+            num += 1;
+        }
+    }
+
+    cout << num;
+}
