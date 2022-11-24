167fd49e3ff003c28f7171293ee693672fe78f81
shang14 <shang14@gmail.com>
2022-11-24 10:46:02 +0800

    new file:   AtCoderBeginnersSelection/ABC081A/main.dart

---
+++ AtCoderBeginnersSelection/ABC081A/main.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  var num = 0;
+  final masu = stdin.readLineSync();
+  for (var i = 0; i < 3; i++) {
+    if (masu[i] == '1') {
+      num += 1;
+    }
+  }
+  print(num);
+}
